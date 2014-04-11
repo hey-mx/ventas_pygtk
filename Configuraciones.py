9f1f92e68383e5ca1d8b68f42765ad1a02240892
Juan Carlos Juarez <jjuarez@jobomas.com>
2014-04-11 09:43:06 -0500

    Agregando configuracion de margenes para la impresion de tickets

--- Configuraciones.py
+++ Configuraciones.py
@@ -4,7 +4,7 @@ pygtk.require("2.0")
 import gtk
 from utils.Form import FormBuilder
 from utils.Database import DataModel
-from utils.Config import get_pdf_reader
+from utils.Config import get_pdf_reader, get_margen
 
 class ConfiguracionesFactory(gtk.Frame):
     
@@ -17,6 +17,8 @@ class ConfiguracionesFactory(gtk.Frame):
         builder.add_from_file("config_frame.glade")
         self.content = builder.get_object("vbox_content")
         self.entry = builder.get_object("pdf_entry")
+        self.entry_margen_superior = builder.get_object("margen_superior")
+        self.entry_margen_izquierdo = builder.get_object("margen_izquierdo")
         builder.connect_signals(self)
         self.content.reparent(self)
         self.content.show()
@@ -24,7 +26,12 @@ class ConfiguracionesFactory(gtk.Frame):
         reader = get_pdf_reader()
         self.entry.set_text(reader['valor'])
         self.__record_id = reader['id']
-    
+        self.margen_izquierdo =  get_margen('margen_izquierdo')
+        self.margen_superior = get_margen('margen_superior')
+        if self.margen_superior is not None:
+            self.entry_margen_superior = self.margen_superior
+        if self.margen_izquierdo is not None:
+            self.entry_margen_izquierdo = self.margen_izquierdo
 
     def on_aceptar_button_clicked(self, widget):
         pdf = self.entry.get_text()
@@ -33,7 +40,38 @@ class ConfiguracionesFactory(gtk.Frame):
         else:
             self.config_model.update_record({'parametro': 'pdf_reader', 'valor': pdf}, self.__record_id)
 
+        try:
+            float(self.entry_margen_superior.get_text())
+        except:
+            self._show_error_message('El valor para el margen superior debe de ser numerico')
+            return
+        if self.margen_superior is None:
+            self.config_model.create_record({'parametro': 'margen_superior', 
+                    'valor': self.entry_margen_superior.get_text()})
+        else:
+            self.config_model.update_record({'valor': self.entry_margen_superior.get_text()},
+                self.margen_superior['id'])
+        
+        try:
+            float(self.entry_margen_izquierdo.get_text())
+        except:
+            self._show_error_message('El valor para el margen izquierdo debe de ser numerico')
+            return
+
+        if self.margen_izquierdo is None:
+            self.config_model.create_record({'parametro': 'margen_izquierdo', 
+                    'valor': self.entry_margen_izquierdo.get_text()})
+        else:
+            self.config_model.update_record({'valor': self.entry_margen_izquierdo.get_text()},
+                self.margen_izquierdo['id'])
+
     def on_cerrar_button_clicked(self, widget):
         page = self.parent.get_current_page()
         self.parent.remove_page(page)
         del self.main.pages[page]
+
+    def _show_error_message(self, message):
+        dialog = gtk.MessageDialog(self.parent.parent.parent, gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, message)
+        dialog.run()
+        dialog.destroy()
--- utils/Config.py
+++ utils/Config.py
@@ -6,3 +6,11 @@ def get_pdf_reader():
     if len(pdf) > 0:
         for item in pdf:
             return item
+
+def get_margen(tipo):
+    config_model = DataModel('Configuracion')
+    margen = config_model.get_records(parametro = tipo)
+    if len(margen) > 0:
+        for item in margen:
+            return item
+    return None
